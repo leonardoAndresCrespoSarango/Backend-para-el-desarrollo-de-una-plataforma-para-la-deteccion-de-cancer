f54b9d0c39b06d23f0ef3cfdf19d8aef5d094976
Leonardo Andrés Crespo Sarango <lcrespos@est.ups.edu.ec>
2024-06-18 10:31:04 -0500

    primer commit

--- app.py
+++ app.py
@@ -1,8 +1,65 @@
 from flask import Flask
-
+from tensorflow.keras.models import load_model
+import tensorflow.keras.backend as K
+import tensorflow as tf
 app = Flask(__name__)
+# dice loss as defined above for 4 classes
+def dice_coef(y_true, y_pred, smooth=1.0):
+    class_num = 5
+    for i in range(class_num):
+        y_true_f = K.flatten(y_true[:,:,:,i])
+        y_pred_f = K.flatten(y_pred[:,:,:,i])
+        intersection = K.sum(y_true_f * y_pred_f)
+        loss = ((2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth))
+   #     K.print_tensor(loss, message='loss value for class {} : '.format(SEGMENT_CLASSES[i]))
+        if i == 0:
+            total_loss = loss
+        else:
+            total_loss = total_loss + loss
+    total_loss = total_loss / class_num
+#    K.print_tensor(total_loss, message=' total dice coef: ')
+    return total_loss
+def dice_coef_necrotic(y_true, y_pred, epsilon=1e-6):
+    intersection = K.sum(K.abs(y_true[:,:,:,1] * y_pred[:,:,:,1]))
+    return (2. * intersection) / (K.sum(K.square(y_true[:,:,:,1])) + K.sum(K.square(y_pred[:,:,:,1])) + epsilon)
+def dice_coef_edema(y_true, y_pred, epsilon=1e-6):
+    intersection = K.sum(K.abs(y_true[:,:,:,2] * y_pred[:,:,:,2]))
+    return (2. * intersection) / (K.sum(K.square(y_true[:,:,:,2])) + K.sum(K.square(y_pred[:,:,:,2])) + epsilon)
+
+def dice_coef_enhancing(y_true, y_pred, epsilon=1e-6):
+    intersection = K.sum(K.abs(y_true[:,:,:,3] * y_pred[:,:,:,3]))
+    return (2. * intersection) / (K.sum(K.square(y_true[:,:,:,3])) + K.sum(K.square(y_pred[:,:,:,3])) + epsilon)
+# Computing Precision
+def precision(y_true, y_pred):
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        return precision
 
 
+# Computing Sensitivity
+def sensitivity(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    return true_positives / (possible_positives + K.epsilon())
+
+
+# Computing Specificity
+def specificity(y_true, y_pred):
+    true_negatives = K.sum(K.round(K.clip((1-y_true) * (1-y_pred), 0, 1)))
+    possible_negatives = K.sum(K.round(K.clip(1-y_true, 0, 1)))
+    return true_negatives / (possible_negatives + K.epsilon())
+try:
+    modelo= load_model("model/modelUnet3D.h5",custom_objects={'accuracy': tf.keras.metrics.MeanIoU(num_classes=5),
+                                                "dice_coef": dice_coef,
+                                                "precision": precision,
+                                                "sensitivity": sensitivity,
+                                                "specificity": specificity},
+                                compile=False)
+    modelo_cargado = True
+except Exception as e:
+    modelo_cargado = False
+    print(f"Error al cargar el modelo: {e}")
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
